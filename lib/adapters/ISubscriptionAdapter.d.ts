7d4e8c821a1934e83b797edf7805bb380508e50f
Alireza Hariri <ali.71hariri@gmail.com>
2020-03-31 21:41:08 +0430

    create temporal release

---
+++ lib/adapters/ISubscriptionAdapter.d.ts
@@ -0,0 +1,13 @@
+import IQueryBuilderOptions from "../IQueryBuilderOptions";
+export default interface IMutationAdapter {
+  subscriptionBuilder: () => {
+    variables: any;
+    query: string;
+  };
+  subscriptionsBuilder: (
+    options: IQueryBuilderOptions[]
+  ) => {
+    variables: any;
+    query: string;
+  };
+}
